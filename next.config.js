e438791d59410613c49e2b7c107b3161a47f827e
Sumit Kumar <sumitkumar@Sumits-MacBook-Air.local>
2025-11-09 01:52:43 +0530

    Fix Node version, remove unsupported ESLint config, static export setup

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
+  output: 'export',       // static export
+  images: { unoptimized: true }, // allow external images
 };
 
 module.exports = nextConfig;
-
